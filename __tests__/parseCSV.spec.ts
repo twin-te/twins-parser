f947d4f5022af7572702daa35a7a17ba4f3eac09
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-13 00:41:49 +0900

    Modify to define new expectToThrow func for test

--- __tests__/parseCSV.spec.ts
+++ __tests__/parseCSV.spec.ts
@@ -1,34 +1,32 @@
 import { parseCsv } from "../src";
 
+function expectToThrow(csv: string) {
+  expect(() => {
+    parseCsv(csv);
+  }).toThrow();
+}
+
 describe("parseCSV", () => {
   it("returns array of string", () => {
-    const csv = "\"AAAAAAA\"\n\"BBBBBBB\"\n\"CCCCCCC\"";
+    const csv = '"AAAAAAA"\n"BBBBBBB"\n"CCCCCCC"';
     const results = parseCsv(csv);
     expect(results).toEqual(["AAAAAAA", "BBBBBBB", "CCCCCCC"]);
   });
 
   it("throws error when csv length is 0", () => {
     const csv = "";
-    expect(() => {
-      parseCsv(csv);
-    }).toThrow();
+    expectToThrow(csv);
   });
 
   it("throws error when parsed item length is not 7", () => {
-    const csv = "\"AAAAAA\"";
-    expect(() => {
-      parseCsv(csv);
-    }).toThrow();
+    const csv = '"AAAAAA"';
+    expectToThrow(csv);
   });
 
   it("throws error when parsed item other than numbers and upper case letters", () => {
-    const csv1 = "\"aaaaaaa\"";
-    const csv2 = "\"これはななもじ\"";
-    expect(() => {
-      parseCsv(csv1);
-    }).toThrow();
-    expect(() => {
-      parseCsv(csv2);
-    }).toThrow();
+    const csv1 = '"aaaaaaa"';
+    const csv2 = '"これはななもじ"';
+    expectToThrow(csv1);
+    expectToThrow(csv2);
   });
 });
