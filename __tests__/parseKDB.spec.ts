01a17e85d8f78e5e20949ce3c49ea30c4b9617ea
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-13 02:53:46 +0900

    Add Todo comments

--- __tests__/parseKDB.spec.ts
+++ __tests__/parseKDB.spec.ts
@@ -1,8 +1,11 @@
 import getDBData from "./utils/getDBData";
+import { parseKDB } from "../src";
 
 describe("parseKDB", () => {
   it("returns array of course", async () => {
-    const h = await getDBData()
-    console.log(h)
+    const csvText = await getDBData()
+    const result = parseKDB(csvText);
+    // TODO: テスト書く
+    expect(result).not.toBe("");
   });
 });
--- src/parseKDB.ts
+++ src/parseKDB.ts
@@ -1,6 +1,8 @@
+// TODO: 講座の型を作る
 type Course = {};
 
 export function parseKDB(csvText: string): Course[] {
   console.log(csvText);
+  // TODO: パーサー書く
   return [];
 }
