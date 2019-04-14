6fedd7a9f94411911d98da6c3defdfe4da97d2b3
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-14 19:13:47 +0900

    Fix implementation of parseKDB

--- src/parseKDB.ts
+++ src/parseKDB.ts
@@ -1,14 +1,13 @@
 import { parseCSV } from "./parseCSV";
 
 type Course = {
-  way: string;
   id: string;
   title: string;
   credit: string;
-  annual: string;
+  annuals: string;
   terms: string;
   periods: string;
-  room: string;
+  rooms: string;
   instructors: string;
   overview: string;
   remark: string;
@@ -16,34 +15,24 @@ type Course = {
 
 export function parseKDB(csvText: string): Course[] {
   const parsed = parseCSV(csvText);
-  const lines = parsed.map((row, i) =>
-    i === 0 ? Object.keys(row) : Object.values(row)
-  );
-  const courses = lines.map(line => {
-    const way = line[0];
-    const id = line[1];
-    const title = line[2];
-    const credit = line[3];
-    const annual = line[4];
-    const terms = line[5];
-    const periods = line[6];
-    const room = line[7];
-    const instructors = line[8];
-    const overview = line[9];
-    const remark = line[10];
-    return {
-      way,
-      id,
-      title,
-      credit,
-      annual,
-      terms,
-      periods,
-      room,
-      instructors,
-      overview,
-      remark
+  const courses: Course[] = [];
+
+  for (const row of parsed) {
+    const course: Course = {
+      id: row[0],
+      title: row[1],
+      credit: row[3],
+      annuals: row[4],
+      terms: row[5],
+      periods: row[6],
+      rooms: row[7],
+      instructors: row[8],
+      overview: row[9],
+      remark: row[10]
     };
-  });
+
+    courses.push(course);
+  }
+
   return courses;
 }
